ac21b37d0801dd9096ca560d794565f652c3d5de
EC2 Default User <ec2-user@ip-172-31-27-68.ap-northeast-1.compute.internal>
2022-08-02 13:20:48 +0000

    migrationファイル編集

--- db/migrate/20220717131753_create_books.rb
+++ db/migrate/20220717131753_create_books.rb
@@ -3,8 +3,7 @@ class CreateBooks < ActiveRecord::Migration[6.1]
     create_table :books do |t|
       t.string :title
       t.string :body
-      t.datetime "created_at", precision: 6, null: false
-      t.datetime "updated_at", precision: 6, null: false
+
       t.timestamps
     end
   end
